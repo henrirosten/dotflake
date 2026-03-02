2974f865b15234ce54dec995214101f66561bc98
Henri Rosten <henri.rosten@unikie.com>
2026-03-02 13:42:03 +0200

    Add vscode extensions and replace bbenoist.nix with nix-ide
    Add github-markdown-preview, gitlens, vscode-yaml, and gremlins
    extensions. Replace bbenoist.nix with jnoortheen.nix-ide for better
    Nix language support with LSP integration.
    
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

--- modules/home/vscode.nix
+++ modules/home/vscode.nix
@@ -6,10 +6,14 @@
     # https://github.com/nix-community/nix-vscode-extensions/blob/master/data/cache/open-vsx-latest.json
     profiles.default.extensions = with pkgs.vscode-extensions; [
       vscodevim.vim
-      bbenoist.nix
+      jnoortheen.nix-ide
       ms-python.python
       mechatroner.rainbow-csv
       shardulm94.trailing-spaces
+      bierner.github-markdown-preview
+      eamodio.gitlens
+      redhat.vscode-yaml
+      nhoizey.gremlins
     ];
   };
 }
