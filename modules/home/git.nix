339444d7e80a259ec0ab2a31cfa157ba6ef6c9cb
Henri Rosten <henri.rosten@unikie.com>
2026-03-02 08:31:40 +0200

    Add Catppuccin Mocha theme for lazygit
    Use programs.lazygit module instead of manually adding the package.
    Configure Catppuccin Mocha (blue accent) theme and set a readable
    author color override for Henri Rosten.
    
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,9 +1,28 @@
 { user }:
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    lazygit
-  ];
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      gui.authorColors = {
+        "Henri Rosten" = "#89b4fa";
+      };
+      gui.theme = {
+        activeBorderColor = [
+          "#89b4fa"
+          "bold"
+        ];
+        inactiveBorderColor = [ "#a6adc8" ];
+        optionsTextColor = [ "#89b4fa" ];
+        selectedLineBgColor = [ "#313244" ];
+        cherryPickedCommitBgColor = [ "#45475a" ];
+        cherryPickedCommitFgColor = [ "#89b4fa" ];
+        unstagedChangesColor = [ "#f38ba8" ];
+        defaultFgColor = [ "#cdd6f4" ];
+        searchingActiveBorderColor = [ "#f9e2af" ];
+      };
+    };
+  };
 
   programs.git = {
     enable = true;
