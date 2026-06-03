65e8467d8f7be50103618160cf0b31eb3ee713dc
Henri Rosten <henri.rosten@unikie.com>
2026-06-03 08:56:05 +0300

    Remove HM sd-switch workaround
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

--- users/hrosten/home.nix
+++ users/hrosten/home.nix
@@ -77,7 +77,4 @@ in
     substituters = https://cache.nixos.org/
     trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
   '';
-  # Ref: https://github.com/nix-community/home-manager/issues/5552
-  # Workaround for HM passing a nonexistent units dir to sd-switch
-  xdg.configFile."systemd/user/.hm-keep".text = "";
 }
